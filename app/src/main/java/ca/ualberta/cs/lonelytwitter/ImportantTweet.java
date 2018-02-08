1ace25439256613a4b9e1f68f9ee13d1531f6b93
AidanKosik <aidankosik@mac.com>
2018-02-08 13:59:22 -0700

    added Watts1 files

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
@@ -1,40 +1,15 @@
 package ca.ualberta.cs.lonelytwitter;
 
-/**
- * Created by dezfuli on 1/16/18.
- */
-import java.util.Date;
+public class ImportantTweet extends Tweet{
 
-/**
- * This class of tweets are important tweets. It extends tweet.
- */
-public class ImportantTweet extends Tweet {
-    /**
-     * Constructor tweet when only taking message as a parameter.
-     * @param message this is the message of the tweet.
-     */
-    ImportantTweet(String message){
+    public ImportantTweet(String message){
         super(message);
     }
 
-    /**
-     * Constructor for important tweet when taking message
-     * and date as parameters.
-     * @param message the message of the tweet
-     * @param date the date of the tweet
-     */
-    ImportantTweet(String message, Date date){
-        super(message, date);
-    }
-
-    /**
-     * Returns if the tweet is important or not.
-     * Since important tweets are always important,
-     * this will always return true.
-     * @return Boolean that is always false.
-     */
     @Override
-    public Boolean isImportant() {
+    public Boolean isImportant(){
         return Boolean.TRUE;
     }
+
+
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
@@ -1,41 +1,11 @@
 package ca.ualberta.cs.lonelytwitter;
 
-/**
- * Created by dezfuli on 1/16/18.
- */
-import java.util.Date;
-
-/**
- * A normal tweet, has no special implementation of getMessage.
- * @author dezfuli
- * @see Tweet
- */
 public class NormalTweet extends Tweet {
-    /**
-     * Constructor for a normal tweet, takes only a message
-     * and uses the tweet classes constructor.
-     * @param message the tweets message
-     */
-    NormalTweet(String message){
+
+    public NormalTweet(String message) {
         super(message);
     }
 
-    /**
-     * Second constructor for a normal tweet, takes two parameters.
-     * One for the message of the tweet and the other is the date.
-     * @param message the message of the tweet
-     * @param date the date of the tweet
-     */
-    NormalTweet(String message, Date date){
-        super(message, date);
-    }
-
-    /**
-     * Returns if the tweet is important or not.
-     * Normal tweets are not important so this
-     * always returns false.
-     * @return Boolean will always return false.
-     */
     @Override
     public Boolean isImportant() {
         return Boolean.FALSE;
